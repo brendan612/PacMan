86ce578b820a69dd8ef4746df08894cc11eae562
David <David@Davids-Desktop>
2017-08-30 12:04:43 -0500

    Ghost class updated to start implementation

--- Pacman/src/pacman/entities/Ghost.java
+++ Pacman/src/pacman/entities/Ghost.java
@@ -5,10 +5,22 @@
  */
 package pacman.entities;
 
+import java.awt.Point;
+import pacman.graphics.GraphicsLoop;
+
 /**
  *
  * @author Bren
  */
-public class Ghost {
+public class Ghost extends Entity {
+
+	public Ghost(GraphicsLoop gl, Point pos) {
+		super(gl, pos);
+	}
+
+	@Override
+	public void tick() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
     
 }
